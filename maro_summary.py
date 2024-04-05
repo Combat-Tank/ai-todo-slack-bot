811a6bec779ac3e631921a030a5b9d0b63b527e3
Manuel Rosa <manuel.rosa@freiheit.com>
2024-04-05 22:24:07 +0100

    updated

--- maro_summary.py
+++ maro_summary.py
@@ -12,20 +12,43 @@ model = VertexAI(model_name="gemini-pro")
 
 
 class Message(BaseModel):
-    user_id: str
+    user: str
+    type: str
     ts: str
     text: str
 
 
-knowledge_base = [
-    Message(user_id="maro", ts="1712340030", text="Welcome the new team member Nils"),
-    Message(user_id="maro", ts="1712340035", text="We have a new team member Max!!"),
-    Message(user_id="maro", ts="1712340038", text="Welcome Ribeiro!!"),
-]
+# knowledge_base = [
+#     Message(user_id="maro", ts="1712340030", text="Welcome the new team member Nils"),
+#     Message(user_id="maro", ts="1712340035", text="We have a new team member Max!!"),
+#     Message(user_id="maro", ts="1712340038", text="Welcome Ribeiro!!"),
+# ]
+
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+import os
+
+SLACK_TOKEN = os.getenv("TOKEN")
+
+client = WebClient(token=SLACK_TOKEN)
+
+Message
+
+channel_id = "C06SYHHSDEH"
+
+messages = []
+try:
+    # Fetch recent messages from a channel
+    response = client.conversations_history(channel=channel_id)
+    messages = response.data["messages"]
+except SlackApiError as e:
+    print(f"Error fetching messages: {e}")
 
 context = ""
-for message in knowledge_base:
-    context += message.text + "\n"
+for message in messages:
+    context += message["text"] + "\n"
+    if len(context) > 5000:
+        break
 
 message = f"Can you give me a summary of what happened today? I have these messages as context: {context}"
 res = model.invoke(message)
